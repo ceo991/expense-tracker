68ef917caa01920c42ba498ed18de707816e4445
ceo991 <ceo991@hotmail.com>
2022-05-07 17:38:19 +0300

    further styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+  const dateArr =props.date.split("-").map(el=>parseInt(el))
+
+  const month =  new Date(dateArr[0],dateArr[1]-1,dateArr[2]).toLocaleString('en-Us',{month:"long"})
+  const day = new Date(dateArr[0],dateArr[1]-1,dateArr[2]).toLocaleString('en-Us',{day:"2-digit"})
+  const year = new Date(dateArr[0],dateArr[1]-1,dateArr[2]).getFullYear()
 
   return (
     <div className='expense-item'>
-      <div>{props.date}</div>
+      <div className='expense-date'>
+        <div className='expense-date__month'>{month}</div>
+        <div className='expense-day'>{day}</div>
+        <div className='expense-year'>{year}</div>
+      </div>
       <div>
         <h2 className='expense-item__description'>{props.description}</h2>
         <p  className='expense-item__price'>{props.price}$</p>
